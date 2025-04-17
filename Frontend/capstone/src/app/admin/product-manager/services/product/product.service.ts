f09964220bd85e8ab22d5a28bb929d1bb63a661b
TSRUTHI <tsruthi@ford.com>
2025-04-17 18:01:57 +0530

    Update product.service.ts

--- Frontend/capstone/src/app/admin/product-manager/services/product/product.service.ts
+++ Frontend/capstone/src/app/admin/product-manager/services/product/product.service.ts
@@ -7,26 +7,27 @@ import { Product } from '../../models/product.model';
   providedIn: 'root'
 })
 export class ProductService {
+  //Base URL To Connect to Backend
   private backendUrl = 'http://localhost:8080/api/v1/admin/products'; 
 
   constructor(private http: HttpClient) {}
-
+  //Get Method to retrieve all products
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.backendUrl}`);
   }
-
+  //Retrieving the Product By product id to view details of product
   getProductById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.backendUrl}/${id}`);
   }
-
+  //Creates a new product on the backend.
   createProduct(product: Omit<Product, 'id'>): Observable<Product> {
     return this.http.post<Product>(`${this.backendUrl}`, product);
   }
-
+  //PUT method to update product details by using product id.
   updateProduct(product: Product): Observable<Product> {
     return this.http.put<Product>(`${this.backendUrl}/${product.id}`, product);
   }
-
+  //Deleting the product by product id
   deleteProduct(productId: number): Observable<any> {
     return this.http.delete(`${this.backendUrl}/${productId}`);
   }
